723cfaf9fc6d16e0c0caec6148ecf1e1dbc62831
Cameron Taggart <cameron.taggart@gmail.com>
2017-09-25 20:51:42 +0300

    build helloworld it on linux

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,11 @@
 
 extern crate octh;
 
-use std::ffi::CString;
-
-// https://github.com/ctaggart/octh/issues/18
 // https://thefullsnack.com/en/string-ffi-rust.html
+use std::ffi::CString;
 
 #[no_mangle]
 pub unsafe extern "C"  fn Ghelloworld (shl: *const octh::root::octave::dynamic_library, relative: bool) -> *mut octh::root::octave_dld_function {
-
-    let fname = CString::new("Fhelloworld").unwrap();
-    let pfname = fname.as_ptr() as octh::root::octave_builtin_fcn;
-    std::mem::forget(pfname);
-
     let name = CString::new("helloworld").unwrap();
     let pname = name.as_ptr() as *const octh::root::std::string;
     std::mem::forget(pname);
@@ -23,17 +16,11 @@ pub unsafe extern "C"  fn Ghelloworld (shl: *const octh::root::octave::dynamic_l
     let pdoc = doc.as_ptr() as *const octh::root::std::string;
     std::mem::forget(pdoc);
 
-//    let fcn = octh::root::octave_dld_function_create(pfname as u64, shl, pname as u64, pdoc as u64);
-//    octh::root::octave_dld_function::create()
-    let fcn = octh::root::octave_dld_function_create(pfname, shl, pname, pdoc);
-
-//    if relative {
-//        fcn.mark_relative();
-//    }
-    return fcn;
+    octh::root::octave_dld_function_create(Some(Fhelloworld), shl, pname, pdoc)
 }    
 
- pub unsafe extern "C"  fn Fhelloworld (args: *const octh::root::octave_value_list, nargout: i32) -> octh::root::octave_value_list {
-     let out = octh::root::octave_value_list_create();
-     return out;
- }
+pub unsafe extern "C" fn Fhelloworld (args: *const octh::root::octave_value_list, nargout: ::std::os::raw::c_int) -> octh::root::octave_value_list {
+    let list_ptr = ::std::ptr::null_mut();
+    octh::root::octave_value_list_new(list_ptr);
+    ::std::ptr::read(list_ptr)
+}
